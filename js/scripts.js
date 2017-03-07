cc064729f30032762accbe7529927c6cf62dc04f
Emily Dokken and Jessica Robertson <jessica@>
2017-03-06 20:54:44 -0800

    it will create a factorial of a number that is inputted

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+//Business Logic
+
+
+
+// var factorial = function(number) {
+//   number * all numbers < number && > 0
+// };
+//  = number - 1 * number > 0
+//Front End Logic
+
+
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var total = 1
+    var factor = parseInt($("input#number").val());
+    for (var i = factor; i >= 1; i --) {
+        total *= i;
+      }
+      alert(total);
+      
+
+
+  });
+});
